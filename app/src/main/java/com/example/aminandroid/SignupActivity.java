35d3c40c7e3b94023677c938f0b7f9a907de6509
aminmasalha7678 <aminjaber8@gmail.com>
2023-10-30 09:58:52 +0200

    nigg
    a

--- app/src/main/java/com/example/aminandroid/SignupActivity.java
+++ app/src/main/java/com/example/aminandroid/SignupActivity.java
@@ -1,16 +1,27 @@
 package com.example.aminandroid;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
+
+import static androidx.fragment.app.FragmentManager.TAG;
 
 public class SignupActivity extends AppCompatActivity {
     EditText email;
@@ -49,8 +60,13 @@ public class SignupActivity extends AppCompatActivity {
             {
                 if (isChecked)
                 {
+
                     adminCode.setVisibility(View.VISIBLE);
                 }
+                else {
+                    adminCode.setVisibility(View.GONE);
+                }
+
 
             }
         });
@@ -58,6 +74,7 @@ public class SignupActivity extends AppCompatActivity {
     }
 
     private void createNewAccount(){
+        error.setVisibility(View.GONE);
         if (email.getText().toString().equals("")){
             error.setVisibility(View.VISIBLE);
             error.setText("Please enter your email");
@@ -80,6 +97,27 @@ public class SignupActivity extends AppCompatActivity {
         }
         else{
             //sign-up with firebase
+            mAuth.createUserWithEmailAndPassword(email.getText().toString(), password.getText().toString())
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                // Sign in success, update UI with the signed-in user's information
+                                String name = username.getText().toString();
+                                FirebaseUser user = mAuth.getCurrentUser();
+                                if(Isadmin.isChecked()){
+                                    name= "admin: "+name;
+                                }
+                                UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder()
+                                        .setDisplayName(name).build();
+                                startActivity(new Intent(SignupActivity.this, MainActivity.class));
+                            } else {
+                                // If sign in fails, display a message to the user.
+                                error.setVisibility(View.VISIBLE);
+                                error.setText(task.getException().getMessage());
+                            }
+                        }
+                    });
         }
     }
 }
