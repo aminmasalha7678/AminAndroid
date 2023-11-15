cb9473c077c2d3fecff9f0bd0ecd3a6f256c5577
aminmasalha7678 <aminjaber8@gmail.com>
2023-11-15 12:10:18 +0200

    sad

--- app/src/main/java/com/example/aminandroid/Classes/Player.java
+++ app/src/main/java/com/example/aminandroid/Classes/Player.java
@@ -1,4 +1,115 @@
 package com.example.aminandroid.Classes;
 
+import java.util.Arrays;
+
 public class Player {
+
+    private String pid;
+    private String name;
+    private Team team;
+    private int age;
+    private int mvps;
+    private int points;
+    private int championships;
+
+    private Byte[] player_img;
+
+    public Player(String pid,String name, Team team, int age, int mvps, int championships, int points, Byte[] player_img) {
+        this.pid = pid;
+        this.name = name;
+        this.team = team;
+        this.age = age;
+        this.mvps = mvps;
+        this.championships = championships;
+        this.points = points;
+        this.player_img = player_img;
+    }
+    public Player(String pid,String name, Team team, int age, int mvps, int championships, int points) {
+        this.pid = pid;
+        this.name = name;
+        this.team = team;
+        this.age = age;
+        this.mvps = mvps;
+        this.championships = championships;
+        this.points = points;
+    }
+
+
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Team getTeam() {
+        return team;
+    }
+
+    public void setTeam(Team team) {
+        this.team = team;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getPid() {
+        return pid;
+    }
+
+    public void setPid(String pid) {
+        this.pid = pid;
+    }
+
+    public int getMvps() {
+        return mvps;
+    }
+
+    public void setMvps(int mvps) {
+        this.mvps = mvps;
+    }
+
+    public int getChampionships() {
+        return championships;
+    }
+
+    public void setChampionships(int championships) {
+        this.championships = championships;
+    }
+
+    public int getPoints() {
+        return points;
+    }
+
+    public void setPoints(int points) {
+        this.points = points;
+    }
+
+    public Byte[] getPlayer_img() {
+        return player_img;
+    }
+
+    public void setPlayer_img(Byte[] player_img) {
+        this.player_img = player_img;
+    }
+    @Override
+    public String toString() {
+        return "Player{" +
+                "name='" + name + '\'' +
+                ", team=" + team +
+                ", age=" + age +
+                ", championships=" + championships +
+                ", mvps=" + mvps +
+                ", points=" + points +
+                ", player_img=" + Arrays.toString(player_img) +
+                '}';
+    }
 }
--- app/src/main/java/com/example/aminandroid/Classes/Team.java
+++ app/src/main/java/com/example/aminandroid/Classes/Team.java
@@ -1,4 +1,82 @@
 package com.example.aminandroid.Classes;
 
+import java.util.Arrays;
+
 public class Team {
+    private String tid;
+    private String name;
+    private Player[] players;
+    private Byte[] team_img;
+    private int championships;
+    private int establishment; // the year the team was established
+
+    public Team(String tid, String name, Player[] players, Byte[] team_img, int championships,int establishment) {
+        this.tid = tid;
+        this.name = name;
+        this.players = players;
+        this.team_img = team_img;
+        this.championships = championships;
+        this.establishment = establishment;
+    }
+
+    public Team(String tid, String name, Player[] players, int championships,int establishment) {
+        this.tid = tid;
+        this.name = name;
+        this.players = players;
+        this.championships = championships;
+        this.establishment = establishment;
+    }
+
+    public String getTid() {
+        return tid;
+    }
+
+    public void setTid(String tid) {
+        this.tid = tid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Player[] getPlayers() {
+        return players;
+    }
+
+    public void setPlayers(Player[] players) {
+        this.players = players;
+    }
+
+    public Byte[] getTeam_img() {
+        return team_img;
+    }
+
+    public void setTeam_img(Byte[] team_img) {
+        this.team_img = team_img;
+    }
+
+    public int getChampionships() {
+        return championships;
+    }
+
+    public void setChampionships(int championships) {
+        this.championships = championships;
+    }
+
+    @Override
+    public String toString() {
+        return "Team{" +
+                "tid='" + tid + '\'' +
+                ", name='" + name + '\'' +
+                ", players=" + Arrays.toString(players) +
+                ", team_img=" + Arrays.toString(team_img) +
+                ", championships=" + championships +
+                '}';
+    }
 }
+
+
