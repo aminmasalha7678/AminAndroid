a0206ca7754c9b7871a02ad8c7364c5e1f65f02a
aminmasalha7678 <aminjaber8@gmail.com>
2023-12-19 09:47:55 +0200

    ll
    ll

--- app/src/main/java/com/example/aminandroid/Admin/AdminFragments/ViewPlayersFragment.java
+++ app/src/main/java/com/example/aminandroid/Admin/AdminFragments/ViewPlayersFragment.java
@@ -2,35 +2,95 @@ package com.example.aminandroid.Admin.AdminFragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.example.aminandroid.Admin.PlayerAdapter;
 import com.example.aminandroid.Classes.Player;
 import com.example.aminandroid.R;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import static java.lang.Integer.parseInt;
+
 
 public class ViewPlayersFragment extends Fragment {
 
-
-
+    DatabaseReference mDatabase;
+    List<Player> Players;
+    PlayerAdapter adapter;
+    RecyclerView recyclerView;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_view_players, container, false);
+        recyclerView = v.findViewById(R.id.players_list);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        mDatabase = FirebaseDatabase.getInstance("https://aminandroid-45afc-default-rtdb.europe-west1.firebasedatabase.app/").getReference();
+        Players = new ArrayList<>();
+        adapter = new PlayerAdapter(inflater.getContext(), Players);
+        recyclerView.setAdapter(adapter);
+        mDatabase.child("Players").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot ds : snapshot.getChildren()) {
+                    Players.add(new Player(String.valueOf(ds.child("tid").getValue()),
+                            String.valueOf(ds.child("name").getValue()),
+                            parseInt(String.valueOf(ds.child("age").getValue())),
+                            parseInt(String.valueOf(ds.child("mvps").getValue())),
+                            parseInt(String.valueOf(ds.child("championships").getValue())),
+                            parseInt(String.valueOf(ds.child("points").getValue()))));
+                }
+                adapter.notifyDataSetChanged();
 
 
-        
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
         return v;
     }
-    public void fillPlayerInfo(){
 
+    public List<Player> fillPlayerInfo() {
+        List<Player> p = new ArrayList<Player>();
+        mDatabase.child("Players").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot ds : snapshot.getChildren()) {
+                    p.add(new Player(String.valueOf(ds.child("tid").getValue()),
+                            String.valueOf(ds.child("name").getValue()),
+                            parseInt(String.valueOf(ds.child("age").getValue())),
+                            parseInt(String.valueOf(ds.child("mvps").getValue())),
+                            parseInt(String.valueOf(ds.child("championships").getValue())),
+                            parseInt(String.valueOf(ds.child("points").getValue()))));
+
+                }
+                Log.d("dsfds",p.get(0).getName());
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+        return p;
     }
 }
--- app/src/main/java/com/example/aminandroid/Admin/PlayerAdapter.java
+++ app/src/main/java/com/example/aminandroid/Admin/PlayerAdapter.java
@@ -4,15 +4,12 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
-import com.example.aminandroid.Admin.AdminFragments.AddPlayerFragment;
 import com.example.aminandroid.Classes.Player;
 import com.example.aminandroid.R;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.List;
 
@@ -21,16 +18,15 @@ import androidx.recyclerview.widget.RecyclerView;
 
 public class PlayerAdapter extends RecyclerView.Adapter<PlayerAdapter.PlayerViewHolder>{
 
-    List<Player> Players;
+    List<Player> PlayersInfoList;
     Context context;
 
-
-    public PlayerAdapter(Context context,List<Player> Players){
+    public PlayerAdapter(Context context,List<Player> PlayersInfoList){
         this.context = context;
-        this.Players = Players;
+        this.PlayersInfoList = PlayersInfoList;
     }
     @Override
-    public PlayerViewHolder onCreateViewHolder( ViewGroup parent, int i) {
+    public PlayerViewHolder onCreateViewHolder(ViewGroup parent, int i) {
 
         View view = LayoutInflater.from(context).inflate(R.layout.listitem, parent, false);
         return new PlayerViewHolder(view);
@@ -39,23 +35,28 @@ public class PlayerAdapter extends RecyclerView.Adapter<PlayerAdapter.PlayerView
 
     @Override
     public void onBindViewHolder(@NonNull PlayerViewHolder holder, int position) {
+        String name = "Name: " + PlayersInfoList.get(position).getName();
+        String age = "Age: " + PlayersInfoList.get(position).getAge();
+        String points = "Points: " + PlayersInfoList.get(position).getPoints();
 
-
+        holder.PlayerName.setText(name);
+        holder.PlayerAge.setText(age);
+        holder.PlayerPoints.setText(points);
     }
 
     @Override
     public int getItemCount() {
-        return Players.size();
+        return PlayersInfoList.size();
     }
 
     public class PlayerViewHolder extends RecyclerView.ViewHolder {
-        TextView PlayerName, PlayerPoints, PlayerChampionShips;
+        TextView PlayerName, PlayerPoints, PlayerAge;
 
         public PlayerViewHolder(@NonNull View itemView) {
             super(itemView);
-            PlayerName = (TextView) itemView.findViewById(R.id.list_player_name);
-            PlayerPoints = (TextView) itemView.findViewById(R.id.list_player_champion);
-            PlayerChampionShips = (TextView) itemView.findViewById(R.id.list_player_points);
+            PlayerName = itemView.findViewById(R.id.list_player_name);
+            PlayerAge = itemView.findViewById(R.id.list_player_age);
+            PlayerPoints = itemView.findViewById(R.id.list_player_points);
         }
     }
 }
