7bef73323f458d9283c829a94d13ea9b346ab8a1
aminmasalha7678 <aminjaber8@gmail.com>
2024-04-03 20:58:16 +0300

    2
    q

--- app/src/main/java/com/example/aminandroid/Classes/Team.java
+++ app/src/main/java/com/example/aminandroid/Classes/Team.java
@@ -1,16 +1,28 @@
 package com.example.aminandroid.Classes;
 
-import java.util.Arrays;
+import static java.lang.Integer.parseInt;
 
-public class Team {
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
+public class Team{
 
     private String tid;
     private String name;
-    private Byte[] team_img;
+    private ArrayList<Byte> team_img;
     private int championships;
     private int establishment; // the year the team was established
 
-    public Team(String name, Byte[] team_img, int championships, int establishment) {
+    public Team(String name, ArrayList<Byte> team_img, int championships, int establishment) {
         this.name = name;
         this.team_img = team_img;
         this.championships = championships;
@@ -44,11 +56,11 @@ public class Team {
     }
 
 
-    public Byte[] getTeam_img() {
+    public ArrayList<Byte> getTeam_img() {
         return team_img;
     }
 
-    public void setTeam_img(Byte[] team_img) {
+    public void setTeam_img(ArrayList<Byte> team_img) {
         this.team_img = team_img;
     }
 
@@ -79,9 +91,96 @@ public class Team {
     public String toString() {
         return "Team{" +
                 ", name='" + name + '\'' +
-                ", team_img=" + Arrays.toString(team_img) +
                 ", championships=" + championships +
                 '}';
+    }
+
+
+    public ArrayList<Player> getAllTeamPlayers() {
+        final String t1Tid = this.getTid();
+        ArrayList<Player> allPlayers = new ArrayList<Player>();
+
+        DatabaseReference mDatabase = FirebaseDatabase.getInstance("https://aminandroid-45afc-default-rtdb.europe-west1.firebasedatabase.app/").getReference();
+        mDatabase.child("Players").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot ds : snapshot.getChildren()) {
+                    Log.d("aaaa", t1Tid);
+                    Log.d("bbbb", String.valueOf(ds.child("tid").getValue()));
+                    if(t1Tid.equals(ds.child("tid").getValue())){
+                        allPlayers.add(new Player(String.valueOf(ds.child("pid").getValue()),
+                                String.valueOf(ds.child("tid").getValue()),
+                                String.valueOf(ds.child("name").getValue()),
+                                parseInt(String.valueOf(ds.child("pace").getValue())),
+                                parseInt(String.valueOf(ds.child("shooting").getValue())),
+                                parseInt(String.valueOf(ds.child("passing").getValue())),
+                                parseInt(String.valueOf(ds.child("dribbling").getValue())),
+                                parseInt(String.valueOf(ds.child("defense").getValue())),
+                                parseInt(String.valueOf(ds.child("physical").getValue())),
+                                String.valueOf(ds.child("position").getValue())));
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+        return allPlayers;
+    }
+
+    public Player[] getBestTeamPlayers(){
+        ArrayList<Player> allPlayers = this.getAllTeamPlayers();
+        Player[] starterPlayers = new Player[5];
+
+        //0:C 1:PF 2:SF 3:SG 4:PG
+        String[] positions = new String[5];
+        positions[0] = "C";
+        positions[1] = "PF";
+        positions[2] = "SF";
+        positions[3] = "SG";
+        positions[4] = "PG";
+        for (int i = 0;i < allPlayers.size();i++){
+            for (int j = 0;j < positions.length;j++){
+                if(positions[j].equals(allPlayers.get(i).getPosition())){
+                    starterPlayers[j] = allPlayers.get(i);
+                }
+            }
+
+        }
+        for (int i = 0;i < allPlayers.size();i++){
+            for (int j = 0;j < positions.length;j++){
+                if(positions[j].equals(allPlayers.get(i).getPosition())){
+                    if(starterPlayers[j].getOverall() < allPlayers.get(i).getOverall())
+                        starterPlayers[j] = allPlayers.get(i);
+                }
+            }
+
+        }
+
+           return starterPlayers;
+    }
+
+    public int[] startGame(Team t2) {
+        int[] scores = new int[2];
+        scores[0] = 40;
+        scores[1] = 40;
+        Player[] bestT1Team = this.getBestTeamPlayers();
+        Player[]  bestT2Team = t2.getBestTeamPlayers();
+        int overAllT1 = 0;
+        int overAllT2 = 0;
+        for (int i = 0;i<bestT1Team.length;i++){
+            overAllT1+=bestT1Team[i].getOverall();
+            overAllT2+=bestT2Team[i].getOverall();
+        }
+        overAllT1 /= 5;
+        overAllT2 /= 5;
+        scores[0] += (int) (overAllT1);
+        scores[1] += (int) (Math.random()*overAllT2);
+
+        return scores;
     }
 }
 
--- app/src/main/java/com/example/aminandroid/ConclusionActivity.java
+++ app/src/main/java/com/example/aminandroid/ConclusionActivity.java
@@ -2,8 +2,9 @@ package com.example.aminandroid;
 
 import static java.lang.Integer.parseInt;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -12,6 +13,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.aminandroid.Classes.Player;
+import com.example.aminandroid.Classes.Team;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -19,6 +21,7 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.database.annotations.NotNull;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -32,15 +35,16 @@ import okhttp3.Request;
 import okhttp3.RequestBody;
 import okhttp3.Response;
 
-public class ConclusionActivity extends AppCompatActivity {
+public class ConclusionActivity extends AppCompatActivity implements View.OnClickListener {
 
     ImageView pick1_image, pick2_image;
     DatabaseReference mDatabase;
     TextView pick1_name, pick2_name, pick1_score, pick2_score, gameHighlights;
     OkHttpClient client;
     Button goBack;
-    String pick1_id, pick2_id, info, responseBody;
+    String pick1_id, pick2_id, info, responseBody, winner;
     Player p1, p2;
+    Team t1, t2;
     int[] scores;
 
 
@@ -59,6 +63,8 @@ public class ConclusionActivity extends AppCompatActivity {
         gameHighlights = findViewById(R.id.conc_highlights);
         goBack = findViewById(R.id.conc_goback);
 
+        goBack.setOnClickListener(this);
+
         pick1_id = getIntent().getStringExtra("pick1");
         pick2_id = getIntent().getStringExtra("pick2");
         info = getIntent().getStringExtra("info");
@@ -70,7 +76,6 @@ public class ConclusionActivity extends AppCompatActivity {
         client = new OkHttpClient();
 
         if (info.equals("player")) {
-            Log.d("aa", "onResponse: " + responseBody);
             mDatabase.child("Players").addValueEventListener(new ValueEventListener() {
                 @Override
                 public void onDataChange(@NonNull DataSnapshot snapshot) {
@@ -104,8 +109,14 @@ public class ConclusionActivity extends AppCompatActivity {
                     scores = p1.startGame(p2);
                     pick1_score.setText(String.valueOf(scores[0]));
                     pick2_score.setText(String.valueOf(scores[1]));
-                    gameHighlights.setText("a");
-                    sendRequest("hello how are you?");
+                    if(scores[0]>scores[1]){
+                        winner = p1.getName();
+                    }
+                    else{
+                        winner = p2.getName();
+                    }
+
+                    sendRequest("write highlights no longer than 60 words for a fictional 1 vs 1 in basketball (until 7 points) between a player named "+ p1.getName() +"and another player named "+ p2.getName()+"which "+ winner +"won and the score was" + scores[0] +"to" +scores[1] +". "+p1.getName()+"stats are"+p1.toString()+"and "+p2.getName()+"stats are" +p2.toString() +"stats are ranked from 0-99 (0 being the worst 99 being the best)");
                 }
 
                 @Override
@@ -116,15 +127,63 @@ public class ConclusionActivity extends AppCompatActivity {
 
 
         }
+        else if (info.equals("team")) {
+            mDatabase.child("Teams").addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                    for (DataSnapshot ds : snapshot.getChildren()) {
+                        if (pick1_id.equals(ds.getKey())) {
+                            t1 = new Team(String.valueOf(ds.child("tid").getValue()),
+                                    String.valueOf(ds.child("name").getValue()),
+                                    parseInt(String.valueOf(ds.child("championships").getValue())),
+                                    parseInt(String.valueOf(ds.child("establishment").getValue())));
+                            pick1_name.setText(t1.getName());
+                        }
+                        else if (pick2_id.equals(ds.getKey())) {
+                            t2 = new Team(String.valueOf(ds.child("tid").getValue()),
+                                    String.valueOf(ds.child("name").getValue()),
+                                    parseInt(String.valueOf(ds.child("championships").getValue())),
+                                    parseInt(String.valueOf(ds.child("establishment").getValue())));
+                            pick2_name.setText(t2.getName());
+                        }
+                    }
+                    scores = t1.startGame(t2);
+                    pick1_score.setText(String.valueOf(scores[0]));
+                    pick2_score.setText(String.valueOf(scores[1]));
+                    if(scores[0]>scores[1]){
+                        winner = t1.getName();
+                    }
+                    else{
+                        winner = t2.getName();
+                    }
+
+                    sendRequest("h");
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
+
+                }
+            });
+        }
     }
 
-    // Method to send HTTP request to GPT-3 API
+    // Method to send HTTP request to GPT-3.5-turbo API
     private void sendRequest(String message) {
         JSONObject json = new JSONObject();
+        JSONArray messagesArray = new JSONArray();
+        JSONObject messageObj = new JSONObject();
+        try {
+            messageObj.put("role", "user");
+            messageObj.put("content", message);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        messagesArray.put(messageObj);
         try {
             json.put("model", "gpt-3.5-turbo");
-            json.put("max_tokens", 50);
-            json.put("prompt", message);
+            json.put("max_tokens", 200);
+            json.put("messages", messagesArray);
         } catch (JSONException e) {
             e.printStackTrace();
         }
@@ -134,7 +193,7 @@ public class ConclusionActivity extends AppCompatActivity {
         Request request = new Request.Builder()
                 .url("https://api.openai.com/v1/chat/completions")
                 .addHeader("Content-Type", "application/json")
-                .addHeader("Authorization", "Bearer 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")
+                .addHeader("Authorization", "Bearer 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")
                 .post(requestBody)
                 .build();
 
@@ -143,7 +202,6 @@ public class ConclusionActivity extends AppCompatActivity {
         client.newCall(request).enqueue(new Callback() {
             @Override
             public void onFailure(@NotNull Call call, @NotNull IOException e) {
-                Log.e("aa", "Request failed");
                 // Handle failure if needed
             }
 
@@ -151,14 +209,38 @@ public class ConclusionActivity extends AppCompatActivity {
             public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
                 if (response.isSuccessful()) {
                     String responseBody = response.body().string();
-                    Log.d("aa", "Response: " + responseBody);
-                    // Process the response here
+                    try {
+                        JSONObject jsonResponse = new JSONObject(responseBody);
+
+                        // Check if the response contains 'choices' array
+                        if (jsonResponse.has("choices")) {
+                            JSONArray choicesArray = jsonResponse.getJSONArray("choices");
+
+                            // Check if the 'choices' array is not empty
+                            if (choicesArray.length() > 0) {
+                                JSONObject choice = choicesArray.getJSONObject(0);
+
+                                // Extract the message content
+                                JSONObject message = choice.getJSONObject("message");
+                                String assistantMessage = message.getString("content");
+                                gameHighlights.setText(assistantMessage);
+                            }
+                        }
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
                 } else {
-                    Log.e("aa", String.valueOf(response));
                     // Handle unsuccessful response
                 }
             }
         });
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v.getId() == R.id.conc_goback){
+            startActivity(new Intent(ConclusionActivity.this, MainActivity.class));
+        }
     }
 }
 
